421caaf4bf11c28c376df08879615c30389cae8f
Luis Alberto De La Torre <lucho.mix34@gmail.com>
2025-03-24 06:20:03 -0500

    feat:api de prueba

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import AwsS3Helper from "./utils/class/aws-s3-helper.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load the env file based on the current environment
+const currentEnv = process.env.NODE_ENV || "development";
+const envFile = path.join(__dirname, `./../.env.${currentEnv}`);
+
+// env variables
+dotenv.config({ path: envFile });
+
+
+// initialize S3
+AwsS3Helper.getInstance();
+
+const DB: string = process.env.MONGO_DB?.replace(
+	"YJvDrRuzDCLyRYqjvUEfpLZsdenkIKIo",
+	process.env.MONGO_DB_PASSWORD || "",
+) as string;
+mongoose
+	.connect(DB, {
+		dbName: "YouCanCode",
+	})
+	.then(() => {
+		// eslint-disable-next-line no-console
+		console.log(`MongoDB connected successfully`);
+	})
+	.catch((err) => {
+		// eslint-disable-next-line no-console
+		console.log(err);
+	});
+
+const PORT = parseInt(process.env.PORT || "3000");
+app.listen(PORT, "0.0.0.0", () => {
+	// eslint-disable-next-line no-console
+	console.log(`Server running on port ${PORT}`);
+});
